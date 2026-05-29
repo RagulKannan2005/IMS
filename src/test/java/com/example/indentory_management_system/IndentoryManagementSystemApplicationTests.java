73da6eef1d9f1d9972f3ade6e169abab2f3a6853
RagulKannan2005 <165671801+RagulKannan2005@users.noreply.github.com>
2026-05-29 10:45:31 +0530

    first commit

---
+++ src/test/java/com/example/indentory_management_system/IndentoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.indentory_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndentoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
