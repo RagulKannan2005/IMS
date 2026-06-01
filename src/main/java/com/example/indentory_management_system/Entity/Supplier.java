c9a0bb49c731c1273f7fe57e78aa9fffaa1e9a99
RagulKannan2005 <165671801+RagulKannan2005@users.noreply.github.com>
2026-06-02 00:34:36 +0530

    feat: implement core inventory management system modules including authentication, suppliers, and purchase orders

---
+++ src/main/java/com/example/indentory_management_system/Entity/Supplier.java
@@ -0,0 +1,62 @@
+package com.example.indentory_management_system.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="supplier")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String supplierName;
+
+    @Column(nullable=false)
+    private String contactPerson;
+
+    @Column(nullable = false,unique=true)
+    private String supplier_email;
+
+    @Column(nullable =false,unique = true)
+    private String supplierPhone;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private Integer status;
+
+    @Column(nullable=false)
+    private LocalDate createDate;
+
+    @Column(nullable=false)
+    private LocalDate updateDate;
+    
+    @PrePersist
+    public void prePersist() {
+        this.createDate = LocalDate.now();
+        this.updateDate = LocalDate.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updateDate = LocalDate.now();
+    }
+    
+}
